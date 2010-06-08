9a2aa6bbeded3ba3025bf8a200047352a3e6c593
jonasareas <jonasareas@903525ad-744d-adf9-4cbe-01cfbe17651e>
2010-06-08 21:07:47 +0000

    Só ajeitando imports...

--- trunk/Simulacao/src/br/ufrj/ad/controller/variaveis/Exponencial.java
+++ trunk/Simulacao/src/br/ufrj/ad/controller/variaveis/Exponencial.java
@@ -1,10 +1,13 @@
 package br.ufrj.ad.controller.variaveis;
 
+import static java.lang.Math.log;
+import static java.lang.Math.random;
+
 public class Exponencial
 {
 
   public static double geraAmostra(double lambda) {
-    return -(Math.log(Math.random())/lambda);
+    return -(log(random())/lambda);
   }
   
 }
